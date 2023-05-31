402fc7760133412b5133d918b778ee9aa1be314a
krittiyaclark <krittiyaclark@gmail.com>
2023-05-31 14:09:39 -0400

    Fixed the New Place input description

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/FormElements/Input";
 
 import "./NewPlace.css";
-import { VALIDATOR_REQUIRE } from "../../shared/util/validators";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isVlaid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isVlaid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
@@ -14,6 +20,15 @@ const NewPlace = () => {
         label="Title"
         validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid title."
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid description."
+        onInput={descriptionInputHandler}
       />
     </form>
   );
--- src/shared/FormElements/Input.js
+++ src/shared/FormElements/Input.js
@@ -1,4 +1,4 @@
-import React, { useReducer } from "react";
+import React, { useEffect, useReducer } from "react";
 
 import "./Input.css";
 import { validate } from "../util/validators";
@@ -30,12 +30,19 @@ const Input = ({
   rows,
   errorText,
   validators,
+  onInput,
 }) => {
   const [inputState, dispatch] = useReducer(inputReducer, {
     value: "",
     isValid: false,
     isTouched: false,
   });
+
+  const { value, isValid } = inputState;
+
+  useEffect(() => {
+    onInput(id, value, isValid);
+  }, [id, value, isValid, onInput]);
 
   const changeHandler = (event) => {
     dispatch({
